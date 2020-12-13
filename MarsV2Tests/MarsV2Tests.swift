64f50df6b745f3d4e6976ba92f78c1469b811875
MagmMe <marcin@magm.me>
2020-12-13 14:09:35 +0100

    Initial Commit

---
+++ MarsV2Tests/MarsV2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  MarsV2Tests.swift
+//  MarsV2Tests
+//
+//  Created by Marcin Magiera on 13/12/2020.
+//
+
+import XCTest
+@testable import MarsV2
+
+class MarsV2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
